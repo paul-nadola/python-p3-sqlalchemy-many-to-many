39303ce160a42c47662e9304d3fa23a9bcfecb8d
Paul Nadola Oyinga <nadolapaul@gmail.com>
2023-05-25 15:09:51 +0300

    Done

--- lib/models.py
+++ lib/models.py
@@ -10,6 +10,14 @@ metadata = MetaData(naming_convention=convention)
 
 Base = declarative_base(metadata=metadata)
 
+game_user = Table(
+    'game_users',
+    Base.metadata,
+    Column('game_id', ForeignKey('games.id'), primary_key=True),
+    Column('user_id', ForeignKey('users.id'), primary_key=True),
+    extend_existing=True,
+)
+
 class Game(Base):
     __tablename__ = 'games'
 
@@ -20,6 +28,8 @@ class Game(Base):
     price = Column(Integer())
 
     reviews = relationship('Review', backref=backref('game'))
+
+    users = relationship('User', secondary=game_user, back_populates='games')
 
     def __repr__(self):
         return f'Game(id={self.id}, ' + \
@@ -34,8 +44,106 @@ class Review(Base):
     comment = Column(String())
     
     game_id = Column(Integer(), ForeignKey('games.id'))
+    user_id = Column(Integer(), ForeignKey('users.id'))
 
     def __repr__(self):
         return f'Review(id={self.id}, ' + \
             f'score={self.score}, ' + \
             f'game_id={self.game_id})'
+
+class User(Base):
+    __tablename__ = 'users'
+
+    id = Column(Integer(), primary_key=True)
+    name = Column(String())
+    created_at = Column(DateTime(), server_default=func.now())
+    updated_at = Column(DateTime(), onupdate=func.now())
+
+    reviews = relationship('Review', backref=backref('user'))
+
+    games = relationship('Game', secondary=game_user, back_populates='users')
+
+    # don't forget your __repr__()!
+    def __repr__(self):
+        return f'User(id={self.id}, ' + \
+            f'name={self.name})'
+    
+#Many-to-Many with an Association Object --> create a GameUser model
+
+# class GameUser(Base):
+#     __tablename__ = "game_users"
+
+#     id = Column(Integer(), primary_key=True)
+#     game_id = Column(ForeignKey('games.id'))
+#     user_id = Column(ForeignKey('users.id'))
+
+#     game = relationship('Game', back_populates='game_users')
+#     user = relationship('User', back_populates='game_users')
+
+#     def __repr__(self):
+#         return f'GameUser(game_id={self.game_id}, ' + \
+#             f'user_id={self.user_id})'
+
+# Bonus: Association Object Models
+
+# class Game(Base):
+#     __tablename__ = 'games'
+
+#     id = Column(Integer(), primary_key=True)
+#     title = Column(String())
+#     genre = Column(String())
+#     platform = Column(String())
+#     price = Column(Integer())
+#     created_at = Column(DateTime(), server_default=func.now())
+#     updated_at = Column(DateTime(), onupdate=func.now())
+
+#     reviews = relationship('Review', back_populates='game')
+#     users = association_proxy('reviews', 'user',
+#         creator=lambda us: Review(user=us))
+
+#     def __repr__(self):
+
+#         return f'Game(id={self.id}, ' + \
+#             f'title={self.title}, ' + \
+#             f'platform={self.platform})'
+
+# class User(Base):
+#     __tablename__ = 'users'
+
+#     id = Column(Integer(), primary_key=True)
+#     name = Column(String())
+#     created_at = Column(DateTime(), server_default=func.now())
+#     updated_at = Column(DateTime(), onupdate=func.now())
+
+#     reviews = relationship('Review', back_populates='user')
+#     games = association_proxy('reviews', 'game',
+#         creator=lambda gm: Review(game=gm))
+
+#     def __repr__(self):
+
+#         return f'User(id={self.id}, ' + \
+#             f'name={self.name})'
+
+# # See the most important piece below:
+
+# class Review(Base):
+#     __tablename__ = 'reviews'
+
+#     id = Column(Integer(), primary_key=True)
+
+#     score = Column(Integer())
+#     comment = Column(String())
+#     created_at = Column(DateTime(), server_default=func.now())
+#     updated_at = Column(DateTime(), onupdate=func.now())
+
+#     game_id = Column(Integer(), ForeignKey('games.id'))
+#     user_id = Column(Integer(), ForeignKey('users.id'))
+
+#     game = relationship('Game', back_populates='reviews')
+#     user = relationship('User', back_populates='reviews')
+
+#     def __repr__(self):
+
+#         return f'Review(id={self.id}, ' + \
+#             f'score={self.score}, ' + \
+#             f'game_id={self.game_id})'
